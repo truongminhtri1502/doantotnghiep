80be1d3fa050b6bec75658469ded297237498764
truongminhtri1502 <truongminhtri347@gmail.com>
2025-07-15 12:36:44 +0700

    test

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 import streamlit as st
 from streamlit.components.v1 import html
-
-# URL of the service running on another port
-other_service_url = "http://localhost:1880/ui"
-
-# Embed the content using an iframe
-iframe_code = f"""
-<iframe src="{other_service_url}" width="100%" height="1280px" style="border:none;"></iframe>
-"""
-html(iframe_code, height=600)
+import subprocess
+import time
+from pyngrok import ngrok
+import json
+import urllib.request
