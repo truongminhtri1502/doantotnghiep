83a300c68871fea685d164527fe323f7c22ffe1b
truongminhtri1502 <truongminhtri347@gmail.com>
2025-07-15 13:18:04 +0700

    deploy

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import time
 from pyngrok import ngrok
 
 ngrok.set_auth_token("2zsz2SxGP86gxDTO8ZXeD49UFiE_3Qt4k3LttMs9HyRJENKcg")  # Replace with your token
-ngrok_tunnel = ngrok.connect(addr="8501", proto="http", bind_tls=True)
-public_url = ngrok_tunnel.public_url
+ngrok_tunnel = ngrok.connect(addr="1880", proto="http", bind_tls=True)
+public_url = ngrok_tunnel.public_url + "/ui"
 print(f"Streamlit app is running at: {public_url}")
 
 iframe_code = f"""
